d8efe01db1a0742628ef77813d77fa65638da11e
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 14:26:37 +0000

    CLIENT: added registration option on login page

--- src/client/app/routes.py
+++ src/client/app/routes.py
@@ -39,13 +39,14 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
-
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
