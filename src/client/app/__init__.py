3d3b43f8fe587d55d98e84516c54de715538dce7
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 11:38:45 +0000

    CLIENT: defined inital user model

--- src/client/app/__init__.py
+++ src/client/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-from app import routes
+
+from app import routes, models
