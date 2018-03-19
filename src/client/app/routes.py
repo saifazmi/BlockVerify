4e1c4a107ef98b7f5557cbc96a18521f3479e012
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 14:05:19 +0000

    CLIENT: restricting routes with login page and handling next page

--- src/client/app/routes.py
+++ src/client/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, url_for, redirect
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, url_for, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import RegistrationForm, LoginForm
 from app.models import User
@@ -7,6 +8,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
@@ -22,7 +24,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
