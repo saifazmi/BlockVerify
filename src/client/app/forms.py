259b66c4ad888c3ae74f5b8e943aeeb42d6f740a
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 12:56:14 +0000

    CLIENT: custom validators for username and email on registration page

--- src/client/app/forms.py
+++ src/client/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,3 +11,16 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    # Custom validators
+    def validate_username(self, username):
+        """Raise a VlidationError if the username already exists"""
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        """Raise a VlidationError if the email already exists"""
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
