8cf089defdb385157bb71d955d0e65b62e908e0d
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 13:54:08 +0000

    CLIENT: added user login session management

--- src/client/app/models.py
+++ src/client/app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,3 +17,8 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- src/client/app/routes.py
+++ src/client/app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, url_for, redirect
+from flask_login import current_user, login_user
 from app import app, db
 from app.forms import RegistrationForm, LoginForm
 from app.models import User
@@ -12,12 +13,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
