cf37bfc7aa2f068ae01945d5b915f4e2880fc69f
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-19 13:43:54 +0000

    CLIENT: connected login page to db with user and pass validation

--- src/client/app/models.py
+++ src/client/app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -13,3 +13,6 @@ class User(db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- src/client/app/routes.py
+++ src/client/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash
+from flask import render_template, flash, url_for, redirect
 from app import app, db
 from app.forms import RegistrationForm, LoginForm
 from app.models import User
@@ -10,9 +10,15 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
