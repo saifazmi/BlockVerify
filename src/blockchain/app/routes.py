84fc703ea1096da82abe93e33b56d3fdab87b220
Saifullah Azmi <sxa320@bham.ac.uk>
2018-03-21 02:55:20 +0000

    API: setup

---
+++ src/blockchain/app/routes.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+from app import app
+
+
+@app.route('/api')
+def api():
+    response = {
+        'api_version': 0.1,
+        'methods': {
+            'GET': [
+                '/api'
+            ],
+            'POST': []
+        }
+    }
+    return jsonify(response)
